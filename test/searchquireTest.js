41b1a9396c1e836caa396b790e55ac8838813fac
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2019-08-12 16:52:59 +0200

    First draft using require module to search files

--- test/searchquireTest.js
+++ test/searchquireTest.js
@@ -5,26 +5,38 @@ var assert = require('chai').assert;
 var searchquire = require('..');
 
 describe('Searchquire tests', function () {
-  describe('Resolving foo', function () {
+  describe('Simple example case', function () {
 
     it('foo is resolved using basePath', function () {
       var foo = searchquire('foo', {
-        basePath: './samples'
+        basePath: './simple-example/samples'
       });
 
       assert.isDefined(foo);
     });
 
-    it('foo is resolved using mocks', function () {
+    it('foo is resolved using mocks folders with file suffix', function () {
       var foo = searchquire('foo', {
-        basePath: './samples',
+        basePath: './simple-example/samples',
         modulePaths: [{
-          basePath: './mocks',
-          fileSufix: 'Mock'
+          type: 'mock',
+          basePath: './simple-example/mocks',
+          fileSuffix: 'Mock.js'
+        }],
+        moduleMocks: [{
+          name: 'path',
+          stub: {
+            basename: function () {
+              return 'basmock';
+            }
+          }
         }]
       });
 
       assert.isDefined(foo);
+      assert.equal(foo.bigBar(), 'BARMOCK');
+      assert.equal(foo.bigRab(), 'RABMOCK');
+      assert.equal(foo.bigBas(), 'BASMOCK');
     });
 
   });
--- test/simple-example/samples/foo.js
+++ test/simple-example/samples/foo.js
@@ -2,17 +2,17 @@ var bar = require('./bar')
 var path = require('path')
 var crypto
 
-function bigBar () {
+function bigBar() {
   // inline require
   return require('./bar').bar().toUpperCase()
 }
 
-function bigRab () {
+function bigRab() {
   // module wide require
   return bar.rab().toUpperCase()
 }
 
-function bigBas (file) {
+function bigBas(file) {
   return path.basename(file).toUpperCase()
 }
 
