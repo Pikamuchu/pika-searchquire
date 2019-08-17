4f391fbea4e7126a1d44e289c92cb019880f4683
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2019-08-17 16:46:27 +0200

    Adding coverage report

--- test/searchquireTest.js
+++ test/searchquireTest.js
@@ -6,28 +6,29 @@ var searchquire = require('..');
 
 describe('Searchquire tests', function() {
   describe('Simple example case', function() {
-    var bar = searchquire('bar', {
-      basePath: './simple-example/samples'
-    });
-
-    var foo = searchquire('foo', {
-      basePath: './simple-example/samples',
-      modulePaths: [
-        {
-          type: 'mock',
-          basePath: './simple-example/mocks',
-          fileSuffix: 'Mock.js'
-        }
-      ]
-    });
 
     it('bar is resolved using basePath', function() {
+      var bar = searchquire('bar', {
+        basePath: './simple-example/samples'
+      });
+
       assert.isDefined(bar);
       assert.equal(bar.bar(), 'bar');
       assert.equal(bar.rab(), 'rab');
     });
 
     it('foo is resolved using mocks folders with file suffix', function() {
+      var foo = searchquire('foo', {
+        basePath: './simple-example/samples',
+        modulePaths: [
+          {
+            type: 'mock',
+            basePath: './simple-example/mocks',
+            fileSuffix: 'Mock.js'
+          }
+        ]
+      });
+
       assert.isDefined(foo);
       assert.equal(foo.bigBar(), 'BARMOCK');
       assert.equal(foo.bigRab(), 'RABMOCK');
@@ -57,10 +58,10 @@ describe('Searchquire tests', function() {
       ]
     };
 
-    var customerMock = searchquire('dw/customer/Customer', dwApiMockConfig);
-    var orderHelpersTest = searchquire('scripts/order/orderHelpers', cartridgeScriptConfig);
-
     it('orderHelpers is resolved using mocks folders with file suffix and require patterns', function() {
+      var customerMock = searchquire('dw/customer/Customer', dwApiMockConfig);
+      var orderHelpersTest = searchquire('scripts/order/orderHelpers', cartridgeScriptConfig);
+
       var customer = new customerMock();
 
       var orders = orderHelpersTest.getOrders(customer);
